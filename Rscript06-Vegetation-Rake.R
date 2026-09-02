23f7ec979065f215113e82d0c38638de2e6ae23c
Karl Lamothe <karl.lamothe07@gmail.com>
2026-09-02 09:21:29 -0400

    Updated veg code

--- Rscript06-Vegetation-Rake.R
+++ Rscript06-Vegetation-Rake.R
@@ -2,12 +2,13 @@
 source("Rscript00-Packages-Theme.R") 
 
 # read csv files
-Site.info <- read.csv("Data/Site-information.csv", header=T)
+Site.info <- read.csv("Data/Site-information(20260902).csv", header=T)
 colnames(Site.info)
 
 Rake.data.full <- read.csv("Data/Rake_data_full.csv", header=T)
 colnames(Rake.data.full)
 str(Rake.data.full)
+Rake.data.full$Year <- as.character(Rake.data.full$Year)
 
 ################################################################################
 # Data Summaries
@@ -17,4 +18,8 @@ aggregate(Rake.data.full$Volume_mL, list(Rake.data.full$Year,
 aggregate(Rake.data.full$Weight_g, list(Rake.data.full$Year,
                                          Rake.data.full$Cell), sum, na.rm=T)
 
-ggplot()
+ggplot(Rake.data.full, aes(x=Species, y=Volume_mL, color=Year))+
+  geom_point(size=2, pch=20) +
+  coord_flip() +
+  facet_grid(Year~Cell) +
+  scale_color_manual(values=c("#134A8E", "#E8291C"))
